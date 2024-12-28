38adb33a99ecc8a2d2713c58d9c17800f300b361
Goutam-11 <goutamkumar.sharmq@gmail.com>
2024-12-28 19:37:00 +0530

    second commit

--- Rancolor/script.js
+++ Rancolor/script.js
@@ -1,8 +1,12 @@
-const arr=["red","green","yellow","pink","blue"]
 
-var random;
+function getRandomColor(){
+    var val1=Math.floor(Math.random()*256);
+    var val2=Math.floor(Math.random()*256);
+    var val3=Math.floor(Math.random()*256);
+    return `rgb(${val1},${val2},${val3})`
+}
 
-for(i=1;i<=arr.length;i++){
-    random = Math.floor(Math.random() * arr.length);
-    document.querySelectorAll(".box")[i-1].style.backgroundColor=arr[random]
+
+for (i = 1; i <= 5; i++) {
+  document.querySelectorAll(".box")[i - 1].style.backgroundColor = getRandomColor();
 }
